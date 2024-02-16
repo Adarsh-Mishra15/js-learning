d0cc88f2dd9e4458a2728887090512be7b494058
Adarsh Mishra <2019adarshmishra@gmail.com>
2024-02-16 17:06:40 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 >1);
+// console.log(2 >=1);
+// console.log(2 <1);
+// console.log(2==1);
+// console.log(2 !=1);
+
+/* Comparsion of different data types */
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true, works different from equality comparision
+
+console.log(undefined ==0); // false for all comparsions
+
+// ===
+
+console.log("2" == 2); // only compare the value
+console.log("2" ===2); // different datatype therefore false
+
